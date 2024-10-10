c49fc43bc412431e36d9438106208b5cab0d7bd1
girish1214 <girish.raj@adani.com>
2024-10-10 15:07:01 +0530

    Update subscription_variables.tf

--- temp/subscription_variables.tf
+++ temp/subscription_variables.tf
@@ -1,19 +1,1 @@
-variable "Managment_subscription_id" {
-  type = string
-}
 
-variable "Managment_tenant_id" {
-  type = string
-}
-
-variable "connectivity_subscription_id" {
-  type = string
-}
-
-variable "connectivity_tenant_id" {
-  type = string
-}
-
-variable "azuread_tenant_id" {
-  type = string
-}
