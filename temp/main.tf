7003da8f91e4a07244977970e3d81997e66c2ca8
girish1214 <girish.raj@adani.com>
2024-10-10 09:49:10 +0530

    Add files via upload

---
+++ temp/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.75.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.29.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  alias = "Managment"
+  features {}
+  use_msi         = false
+  subscription_id = "d326a752-74a2-4442-8813-abc9087e7813"
+  tenant_id       = "5ee36159-43e4-4b5d-a15b-076176333df1"
+}
+ 
+provider "azurerm" {
+  alias = "connectivity"
+  features {}
+  use_msi         = false
+  subscription_id = ""
+  tenant_id       = "5ee36159-43e4-4b5d-a15b-076176333df1"
+}
+provider "azuread" {
+  tenant_id = "5ee36159-43e4-4b5d-a15b-076176333df1"
+}
+
+#KEYVAULT
+module "key_vault" {
+  source              = "./Modules/key_vault"
+  providers = {
+    azurerm = azurerm.Managment
+  }
+  key_vault_variables = var.key_vault_variables
+}
