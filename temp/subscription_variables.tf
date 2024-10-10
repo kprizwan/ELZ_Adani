06011b369004956d3c3c8d48c24e70ddbb19381b
girish1214 <girish.raj@adani.com>
2024-10-10 13:54:01 +0530

    Add files via upload

---
+++ temp/subscription_variables.tf
@@ -0,0 +1,19 @@
+variable "Managment_subscription_id" {
+  type = string
+}
+
+variable "Managment_tenant_id" {
+  type = string
+}
+
+variable "connectivity_subscription_id" {
+  type = string
+}
+
+variable "connectivity_tenant_id" {
+  type = string
+}
+
+variable "azuread_tenant_id" {
+  type = string
+}
