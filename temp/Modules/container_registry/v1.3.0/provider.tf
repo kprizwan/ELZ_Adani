f16487a7b657a0fe2abca556ed8b852180f0c471
vpalem09 <venkateswara.palem@adani.com>
2024-10-11 13:23:52 +0530

    Update provider.tf

--- temp/Modules/container_registry/v1.3.0/provider.tf
+++ temp/Modules/container_registry/v1.3.0/provider.tf
@@ -1,9 +1,12 @@
 terraform {
   required_providers {
     azurerm = {
-      source                = "hashicorp/azurerm"
-      version               = "3.75.0"
-      configuration_aliases = [azurerm.management]
+      source  = "hashicorp/azurerm"
+      version = "3.75.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.29.0"
     }
   }
 }
