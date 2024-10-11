0f54a8628c4816c70610d8c97fd6377cfc8ac365
girish1214 <girish.raj@adani.com>
2024-10-11 17:53:11 +0530

    Update variables.tf

--- temp/variables.tf
+++ temp/variables.tf
@@ -62,7 +62,7 @@ variable "private_dns_zone_variables" {
 }
 
 #PRIVATE ENDPOINT VARIABLES
-variable "private_endpoint_variables" {
+/*variable "private_endpoint_variables" {
   type = map(object({
     private_endpoint_name                                = string #(Required) Specifies the Name of the Private Endpoint. Changing this forces a new resource to be created.
     private_endpoint_resource_group_name                 = string #(Required) Specifies the Name of the Resource Group within which the Private Endpoint should exist. Changing this forces a new resource to be created.
@@ -101,7 +101,7 @@ variable "private_endpoint_variables" {
   }))
   description = "Map of private endpoint objects. name, subnet_id, is_manual_connection, private_connection_resource_id and subresource_names supported"
   default     = {}
-}
+}*/
 
 
 # CONTAINER REGISTRY VARIABLES
