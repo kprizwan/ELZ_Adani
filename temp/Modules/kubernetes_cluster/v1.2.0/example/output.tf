16c043801ff25b2d26dc5c4611a54a7368f4c48f
vpalem09 <venkateswara.palem@adani.com>
2024-10-11 20:01:43 +0530

    Delete temp/Modules/kubernetes_cluster/v1.2.0 directory

--- temp/Modules/kubernetes_cluster/v1.2.0/example/output.tf
+++
@@ -1,5 +0,0 @@
-#kubernetes cluster output
-output "kubernetes_cluster_output" {
-  value       = module.kubernetes_cluster.kubernetes_cluster_output
-  description = "The Kubernetes cluster output"
-}
--- temp/Modules/kubernetes_cluster/v1.2.0/output.tf
+++
@@ -1,9 +0,0 @@
-#Kubernetes cluster output
-output "kubernetes_cluster_output" {
-  description = "The Kubernetes cluster output"
-  value = { for k, v in azurerm_kubernetes_cluster.kubernetes_cluster : k => {
-    id       = v.id
-    identity = v.identity
-    }
-  }
-}
