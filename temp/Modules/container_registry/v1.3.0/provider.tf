cdbd74ccbb27936b56589caf2d77807ff18faa92
girish1214 <girish.raj@adani.com>
2024-10-11 12:42:26 +0530

    Update provider.tf

--- temp/Modules/container_registry/v1.3.0/provider.tf
+++ temp/Modules/container_registry/v1.3.0/provider.tf
@@ -3,7 +3,7 @@ terraform {
     azurerm = {
       source                = "hashicorp/azurerm"
       version               = "3.75.0"
-      configuration_aliases = [azurerm.management, azurerm.connectivity]
+      configuration_aliases = [azurerm.management]
     }
   }
 }
