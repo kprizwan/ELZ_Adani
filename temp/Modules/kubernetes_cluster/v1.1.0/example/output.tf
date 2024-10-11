74dc52c7e26e57bdbd66d0775c49f16a7da82c40
vpalem09 <venkateswara.palem@adani.com>
2024-10-11 20:01:56 +0530

    Delete temp/Modules/kubernetes_cluster/v1.1.0 directory

--- temp/Modules/kubernetes_cluster/v1.1.0/example/output.tf
+++
@@ -1,8 +0,0 @@
-output "kubernetes_cluster_ids" {
-  value = module.kubernetes_cluster.kubernetes_cluster_ids
-}
-
-output "kubernetes_cluster_principle_ids" {
-  value = module.kubernetes_cluster.kubernetes_cluster_principle_ids
-}
-
