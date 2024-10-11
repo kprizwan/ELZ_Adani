97abe290cf81d78d08f579ea93f6e65292c11d0a
girish1214 <girish.raj@adani.com>
2024-10-11 18:01:58 +0530

    Update main.tf

--- temp/main.tf
+++ temp/main.tf
@@ -57,7 +57,7 @@ module "private_dns_zone" {
 module "private_endpoint" {
   source = "./Modules/private_endpoint/v1.3.0"
   providers = {
-    azurerm = azurerm.management
+    azurerm.management = azurerm.management
   }
   private_endpoint_variables = var.private_endpoint_variables
   depends_on                 = [module.key_vault, module.private_dns_zone]
