99b583ee75b0e871ca739712429db173ad33fd20
girish1214 <girish.raj@adani.com>
2024-10-11 19:28:16 +0530

    Update provider.tf

--- temp/Modules/container_registry/v1.3.0/provider.tf
+++ temp/Modules/container_registry/v1.3.0/provider.tf
@@ -3,7 +3,7 @@ terraform {
     azurerm = {
       source                = "hashicorp/azurerm"
       version               = "3.75.0"
-      configuration_aliases = [azurerm.management, azurerm.container_registry_sub]
+      configuration_aliases = [azurerm.key_vault_sub, azurerm.container_registry_sub]
     }
   }
 }
