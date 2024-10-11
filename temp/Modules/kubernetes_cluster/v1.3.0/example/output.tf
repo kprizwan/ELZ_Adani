a467b22d037d31a9e0708dc9fa137a9224419c09
girish1214 <girish.raj@adani.com>
2024-10-11 20:07:50 +0530

    Delete temp/Modules/kubernetes_cluster/v1.3.0/example directory

--- temp/Modules/kubernetes_cluster/v1.3.0/example/output.tf
+++
@@ -1,5 +0,0 @@
-#KUBERNETES CLUSTER OUTPUT
-output "kubernetes_cluster_output" {
-  value       = module.kubernetes_cluster.kubernetes_cluster_output
-  description = "The Kubernetes cluster output"
-}
