1b59df6964b0e6969ac6eaade81979ed314ece0f
girish1214 <girish.raj@adani.com>
2024-10-11 18:24:42 +0530

    Update provider.tf

--- temp/Modules/aks/v1.1.0/provider.tf
+++ temp/Modules/aks/v1.1.0/provider.tf
@@ -4,10 +4,8 @@ terraform {
       source  = "hashicorp/azurerm"
       /*version = "=3.9.0"*/
       version = ">=3.21"
+    configuration_aliases = [azurerm.management]
     }
   }
 }
 
-provider "azurerm" {
-  features {} 
-}
