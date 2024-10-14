2fa6e380efbb65464e2c23c7101bed3747381eeb
girish1214 <girish.raj@adani.com>
2024-10-14 16:53:42 +0530

    Update provider.tf

--- connectivity/provider.tf
+++ connectivity/provider.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
   alias = "connectivity"
   features {}
   use_msi                    = true
-  #skip_provider_registration = true
+  skip_provider_registration = true
   subscription_id            = "4d4b41f0-5e56-49da-9bc1-713a4a21ddf1"
 }
 
@@ -24,7 +24,7 @@ provider "azurerm" {
   alias = "management"
   features {}
   use_msi                    = true
-  #skip_provider_registration = true
+  skip_provider_registration = true
   subscription_id            = "d326a752-74a2-4442-8813-abc9087e7813"
 
 }
