c3269533be20dd383c7e6b8d5c00e28f96932694
vpalem09 <venkateswara.palem@adani.com>
2024-10-14 16:38:16 +0530

    Delete Azure/eventhub/v1.2.0 directory

--- Azure/eventhub/v1.2.0/example/output.tf
+++
@@ -1,5 +0,0 @@
-# eventhub output
-output "eventhub_output" {
-  description = "Eventhub output values"
-  value       = module.eventhub.eventhub_output
-}
--- Azure/eventhub/v1.2.0/output.tf
+++
@@ -1,9 +0,0 @@
-# eventhub output
-output "eventhub_output" {
-  description = "Eventhub output values"
-  value = { for k, v in azurerm_eventhub.eventhub : k => {
-    id            = v.id
-    partition_ids = v.partition_ids
-    }
-  }
-}
