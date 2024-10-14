26b5cc317a282bd5ee4135ad8086b8d804873ce3
vpalem09 <venkateswara.palem@adani.com>
2024-10-14 16:42:53 +0530

    Delete Azure/eventhub_namespace/v1.1.0 directory

--- Azure/eventhub_namespace/v1.1.0/example/output.tf
+++
@@ -1,5 +0,0 @@
-output "eventhub_namespace_ids" {
-  description = "Eventhub namespace ID"
-  value       = module.eventhub_namespace.eventhub_namespace_ids
-}
-
--- Azure/eventhub_namespace/v1.1.0/output.tf
+++
@@ -1,4 +0,0 @@
-output "eventhub_namespace_ids" {
-  value = { for k, v in azurerm_eventhub_namespace.eventhub_namespace : k => v.id }
-}
-
