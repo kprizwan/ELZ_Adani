da7a023026b471a2d8bd87202033f730948efbf6
girish1214 <girish.raj@adani.com>
2024-10-14 15:04:25 +0530

    Update main.tf

--- connectivity/main.tf
+++ connectivity/main.tf
@@ -9,3 +9,13 @@ module "resource_group" {
   resource_group_variables = var.resource_group_variables
 }
 
+#VIRTUAL NETWORK
+/*module "virtual_network" {
+  source                    = "../Azure/virtual_network/v1.3.0"
+   providers = {
+    azurerm = azurerm.connectivity
+  }
+  virtual_network_variables = var.virtual_network_variables
+  depends_on                = [module.network_security_group]
+}*/
+
