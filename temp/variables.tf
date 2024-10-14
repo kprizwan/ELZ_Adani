825df7e774476438a9e0c926243a9e1b6dcd9d68
girish1214 <girish.raj@adani.com>
2024-10-14 11:52:01 +0530

    Update variables.tf

--- temp/variables.tf
+++ temp/variables.tf
@@ -536,3 +536,18 @@ variable "kubernetes_cluster_variables" {
   }))
   default = {}
 }
+
+############## Connectivity Subscription Resources #####################################
+
+#RESOURCE GROUP VARIABLES
+variable "resource_group_variables" {
+  type = map(object({
+    resource_group_name       = string      #(Required) Name of the Resource Group with which it should be created.
+    resource_group_location   = string      #(Required) The Azure Region where the Resource Group should exist.
+    resource_group_managed_by = string      #(optional)  The ID of the resource or application that manages this Resource Group.
+    resource_group_tags       = map(string) #(Optional) A mapping of tags which should be assigned to the Resource Group.
+  }))
+  description = "Map of Resource groups"
+  default = {
+  }
+}
