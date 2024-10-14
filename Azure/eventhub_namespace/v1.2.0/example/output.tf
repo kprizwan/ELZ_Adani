9500a15daa7889ec669190887d276ec786d7b904
vpalem09 <venkateswara.palem@adani.com>
2024-10-14 16:43:04 +0530

    Delete Azure/eventhub_namespace/v1.2.0 directory

--- Azure/eventhub_namespace/v1.2.0/example/output.tf
+++
@@ -1,4 +0,0 @@
-output "eventhub_namespace_output" {
-  description = "Eventhub namespace output values"
-  value       = module.eventhub_namespace.eventhub_namespace_output
-}
--- Azure/eventhub_namespace/v1.2.0/output.tf
+++
@@ -1,4 +0,0 @@
-output "eventhub_namespace_output" {
-  description = "EventHub Namespace output values"
-  value       = { for k, v in azurerm_eventhub_namespace.eventhub_namespace : k => { id = v.id } }
-}
