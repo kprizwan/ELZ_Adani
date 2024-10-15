bd57ffc19476a8fa28da77214ca74e3e82b0788c
vpalem09 <venkateswara.palem@adani.com>
2024-10-15 10:50:09 +0530

    Delete connectivity/public_ip directory

--- connectivity/public_ip/v1.3.0/output.tf
+++
@@ -1,9 +0,0 @@
-#PUBLIC IP OUTPUTS
-output "public_ip_output" {
-  value = { for k, v in azurerm_public_ip.public_ip : k => {
-    id         = v.id
-    ip_address = v.ip_address
-    fqdn = v.fqdn }
-  }
-  description = "Public IP outputs"
-}
