1fbc235107be7a4e2f03429102ddf181593ba99c
vpalem09 <venkateswara.palem@adani.com>
2024-11-01 18:13:24 +0530

    Update main.tf

--- dev_env/dev-hrgpt-rg/main.tf
+++ dev_env/dev-hrgpt-rg/main.tf
@@ -1,8 +1,12 @@
-#RESOURCE GROUP
+# RESOURCE GROUP MODULE
 module "resource_group" {
-  source = "../Azure/resource_group/v1.3.0"
+  # Source now points to the Git repository path for the module
+  source = "git::https://github.com/kprizwan/ELZ_Adani.git//Azure/resource_group/v1.3.0?ref=sd-branch-dev"
+  
   providers = {
     azurerm = azurerm.dev_env
   }
+  
+  # Pass in the required variables for the resource group module
   resource_group_variables = var.resource_group_variables
 }
