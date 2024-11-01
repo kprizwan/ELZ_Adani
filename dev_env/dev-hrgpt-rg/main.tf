3b5f72a05a6188c52020ef7b41c513bda3f0f5db
Venkateswara Palem <IBMI203031@adani.com>
2024-11-01 18:18:50 +0530

    adding hrgpt to dev

--- dev_env/dev-hrgpt-rg/main.tf
+++ dev_env/dev-hrgpt-rg/main.tf
@@ -1,12 +1,8 @@
-# RESOURCE GROUP MODULE
+#RESOURCE GROUP
 module "resource_group" {
-  # Source now points to the Git repository path for the module
-  source = "git::https://github.com/kprizwan/ELZ_Adani.git//Azure/resource_group/v1.3.0?ref=sd-branch-dev"
-  
+  source = "../../Azure/resource_group/v1.3.0"
   providers = {
     azurerm = azurerm.dev_env
   }
-  
-  # Pass in the required variables for the resource group module
   resource_group_variables = var.resource_group_variables
 }
